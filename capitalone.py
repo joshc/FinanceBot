85a9739d428c1f95d0766746667cb72a94ffe7ec
Victor Chen <vlchen888@gmail.com>
2016-11-12 10:16:56 -0800

    sorts atms, finds location of closest atm

--- capitalone.py
+++ capitalone.py
@@ -10,12 +10,25 @@ def coordinates(address):
 		'address': address
 	});
 
-	return eval(result);
+	return result;
 
 def find_atms(lat, lng, rad):
-	req_url = 'http://api.reimaginebanking.com/atms?lat=' + str(lat) + '&lng=' + str(lng) + '&rad=' + str(rad) + '&key=22827101aa2aed5f76342f276b3e38fb'
-	r = requests.get(req_url)
-	return json.dumps(r.json(), indent=2)
+	url = 'http://api.reimaginebanking.com/atms'
+	apiKey = '22827101aa2aed5f76342f276b3e38fb'
+	payload = {   
+	    "lat": lat,
+	    "lng": lng,
+	    "accessibility": True,
+	    "rad": 1,
+	    "key": apiKey
+	  }
+	r = requests.get(url, params = payload)
+	arr = r.json()[u'data']
+	for dic in arr:
+		dic['dist'] = (dic[u'geocode'][u'lat'] - lat)**2 + (dic[u'geocode'][u'lng'] - lng)**2
+	sort = sorted(arr, key=lambda dic: dic['dist'])
+	return sort
+
 
 def main():
 	debug = 1;
@@ -26,3 +39,14 @@ def main():
 
 	coords = coordinates(address)
 	atms = find_atms(coords['lat'], coords['lng'], raw_input("Enter the radius: "))
+	#for atm in atms:
+	#	print atm['dist'], atm[u'_id'], '(', atm[u'geocode'][u'lat'], ', ', atm[u'geocode'][u'lng'], ')'
+	
+	best_atm = atms[0]
+	address = best_atm[u'address']
+	location = "%s %s, %s %s %s" % (address[u'street_number'], address[u'street_name'], address[u'city'],\
+	 address[u'state'], address[u'zip'], )
+
+	print location
+main()
+	
